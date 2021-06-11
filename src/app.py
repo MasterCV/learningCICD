8de675ab99fde43231bd42a423f61811f155e78e
chetan <chetan.v@simformsolutions.com>
2021-06-11 16:11:49 +0530

    Added Dockerfile

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return "Hello World"
+    # return render_template("solid/index.html")
+    return "HEllo World. With the New Changes"
 
 
 if __name__ == "__main__":
